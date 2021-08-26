333dc801dd170bd639900d71f270e64791c464b6
Hakbar0 <haseeb2000@hotmail.com>
2021-08-26 12:10:24 +0100

    another kata

---
+++ 8kyu/multipleOfIndex.js
@@ -0,0 +1,12 @@
+function multipleOfIndex(array, output=[]) {
+    for(let i =0; i < array.length; i++){
+     if (Number.isInteger(array[i]/i)) output.push(array[i]);  
+    }
+    return output;
+  }
+
+  // better solution
+
+  function multipleOfIndex(array) {
+    return array.filter((num, index) => num % index === 0);
+  }
