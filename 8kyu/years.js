89349e6d2fd41ab4d648d5e0183dc289b7860746
Hakbar0 <haseeb2000@hotmail.com>
2021-08-27 11:38:25 +0100

    another kata

---
+++ 8kyu/years.js
@@ -0,0 +1,19 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let dog = 0, cat = 0;
+    
+    if(humanYears >= 3){
+      dog = 15 + 9 + 4*(humanYears-2);
+      cat = 15 + 9 + 5*(humanYears-2);
+    }
+    
+    else if(humanYears > 1){
+      dog = 15 + 9;
+      cat = 15 + 9;
+    }
+    
+    else {
+      dog = 15;
+      cat = 15;
+    }
+    return [humanYears,dog,cat];
+  }
