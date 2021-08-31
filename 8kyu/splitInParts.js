cbfcd9812cf9601d30297053bae63c196e04ad42
hakbar0 <haseeb2000@hotmail.com>
2021-08-31 12:49:08 +0100

    another kata

---
+++ 8kyu/splitInParts.js
@@ -0,0 +1,6 @@
+var splitInParts = function (s, partLength, arr = []) {
+  for (let i = 0; i < s.length; i += partLength) {
+    arr.push(s.slice(i, i + partLength));
+  }
+  return arr.join(" ");
+};
