ec388e263ab79a933e10d020a24c4d1ebd4321a1
hakbar0 <haseeb2000@hotmail.com>
2021-09-14 21:06:38 +0100

    just comited next kata

---
+++ 8kyu/evenLast.js
@@ -0,0 +1,9 @@
+function evenLast(numbers, sum = 0) {
+  if (numbers.length == 0) return 0;
+  for (var i = 0; i < numbers.length; i++) {
+    if (i % 2 == 0) {
+      sum += numbers[i];
+    }
+  }
+  return sum * numbers[numbers.length - 1];
+}
