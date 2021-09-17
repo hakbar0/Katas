248956cef8210ebf2d2affc95a140179615fec94
hakbar0 <haseeb2000@hotmail.com>
2021-09-17 22:09:21 +0100

    another kata

--- 8kyu/shorterConcat.js
+++ 8kyu/shorterConcat.js
@@ -1,5 +1,6 @@
 function shorter_reverse_longer(a, b) {
-  return b.length <= a.length
+  const isAlonger = b.length <= a.length;
+  return isAlonger
     ? b + a.split("").reverse().join("") + b
     : a + b.split("").reverse().join("") + a;
 }
